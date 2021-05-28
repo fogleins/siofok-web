7b4faa7c7c0653bf33783eda4697d49d0df46621
fogleins <fogleins@hotmail.hu>
2021-05-29 01:09:25 +0200

    Minor visual improvements

--- playlist.php
+++ playlist.php
@@ -20,22 +20,22 @@
     }
 ?>
 
-<h2 class="text-primary text-center" id="playlist-h2">Idei kooperatív lista</h2>
+<h2 class="text-primary text-center h2-top-center">Idei kooperatív lista</h2>
 <div class="container playlist-div">
     <div class="floating-div playlist-div">
         <h4 class="text-primary">Zene hozzáadása:</h4>
         <ol>
-            <li class="add-songs-steps-li">Kattints a linkre!</li>
-            <li class="add-songs-steps-li">Megjelenik a lista oldala. Kattints a követésre (szív ikon).</li>
-            <li class="add-songs-steps-li">A lista bekerül a sajátjaid közé, így már hozzá tudsz adni számokat.</li>
+            <li class="list-item">Kattints a linkre!</li>
+            <li class="list-item">Megjelenik a lista oldala. Kattints a követésre (szív ikon).</li>
+            <li class="list-item">A lista bekerül a sajátjaid közé, így már hozzá tudsz adni számokat.</li>
         </ol>
         <?php
         $db = Utils::getDbObject();
         try {
             $link = $db->query("SELECT link FROM playlist WHERE playlist_ID = 1");
             if ($link->num_rows > 0) {
-                echo "<a href='" . $link->fetch_row()[0]
-                    . "' role='button' id='playlist-btn' class='btn btn-primary btn-lg'>Tovább a listához</a>";
+                echo "<a href='" . $link->fetch_row()[0] . "' role='button' id='playlist-btn'"
+                    . " class='btn btn-primary btn-lg' target='_blank'>Tovább a listához</a>";
             } else {
                 Utils::logEvent(LogType::ERROR(), "Cannot read playlist link");
             }
