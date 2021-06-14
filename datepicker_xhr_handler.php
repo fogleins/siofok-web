c02aece577f3c09b734c2b30fa434a78222bdbee
fogleins <fogleins@hotmail.hu>
2021-06-14 16:50:51 +0200

    Fix date not being checked when editing a previously added date in the datepicker

--- datepicker_xhr_handler.php
+++ datepicker_xhr_handler.php
@@ -16,20 +16,7 @@
         // if a new record will be saved
         if (isset($_POST["recordId"]) && $_POST["recordId"] == null) {
             // make sure that the user cannot add the same range multiple times
-            $stmt = $db->prepare("SELECT response_ID FROM datepicker_responses WHERE user_ID = ? "
-                . "AND start_date BETWEEN ? AND ? OR end_date BETWEEN ? AND ? "
-                . "OR ? BETWEEN start_date AND end_date OR ? BETWEEN start_date AND end_date");
-            $stmt->bind_param("issssss", $_SESSION["userId"], $_POST["start"], $_POST["end"],
-                $_POST["start"], $_POST["end"], $_POST["start"], $_POST["end"]);
-            if ($stmt->execute()) {
-                if ($stmt->get_result()->num_rows > 0) {
-                    echo json_encode(array("success" => false, "message" => "A megadott időszak átfedésben van egy "
-                        . " korábban megadottal, így nem kerül mentésre."));
-                    exit();
-                }
-                $stmt->free_result();
-            } else {
-                echo json_encode(array("success" => false));
+            if (!inputIsValidDateRange($db)) {
                 exit();
             }
 
@@ -61,16 +48,44 @@
             echo json_encode(array("success" => $stmt->execute()));
         } else if (isset($_GET["action"]) && $_GET["action"] == "query") {
             $result = $db->query("SELECT response_ID, start_date, end_date FROM datepicker_responses "
-                . " WHERE user_ID = " . $_SESSION['userId']);
+                . "WHERE user_ID = " . $_SESSION['userId']);
             $result = $result->fetch_all(MYSQLI_ASSOC);
             echo json_encode(array("success" => $result != false, "data" => $result));
         } else /* if editing the range */ {
-            $stmt = $db->prepare("UPDATE datepicker_responses SET start_date = ?, end_date = ? WHERE response_ID = ?");
-            $stmt->bind_param("ssi", $_POST["start"], $_POST["end"], $_POST["recordId"]);
-            echo json_encode(array("success" => $stmt->execute()));
+            if (inputIsValidDateRange($db)) {
+                $stmt = $db->prepare("UPDATE datepicker_responses SET start_date = ?, end_date = ? "
+                    . "WHERE response_ID = ?");
+                $stmt->bind_param("ssi", $_POST["start"], $_POST["end"], $_POST["recordId"]);
+                echo json_encode(array("success" => $stmt->execute()));
+            }
         }
     } catch (Exception $exception) {
         Utils::logEvent(LogType::ERROR(), "Datepicker handler error: " . $exception->getMessage());
     } finally {
         $db->close();
+    }
+
+    /**
+     * Checks whether the input value overlaps with a previously submitted date range.
+     * @param $db: The database object.
+     * @return bool False if dates overlap, true otherwise.
+     */
+    function inputIsValidDateRange(mysqli $db): bool {
+        $stmt = $db->prepare("SELECT response_ID FROM datepicker_responses WHERE user_ID = ? "
+            . "AND start_date BETWEEN ? AND ? OR end_date BETWEEN ? AND ? "
+            . "OR ? BETWEEN start_date AND end_date OR ? BETWEEN start_date AND end_date");
+        $stmt->bind_param("issssss", $_SESSION["userId"], $_POST["start"], $_POST["end"],
+            $_POST["start"], $_POST["end"], $_POST["start"], $_POST["end"]);
+        if ($stmt->execute()) {
+            if ($stmt->get_result()->num_rows > 0) {
+                echo json_encode(array("success" => false, "message" => "A megadott időszak átfedésben van egy "
+                    . "korábban megadottal, így nem kerül mentésre."));
+                return false;
+            }
+            $stmt->free_result();
+            return true;
+        } else {
+            echo json_encode(array("success" => false));
+            return false;
+        }
     }
