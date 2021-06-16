0be352b5978985d5521d375de52df6101581e590
fogleins <fogleins@hotmail.hu>
2021-06-16 19:27:48 +0200

    Add content to the main page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     session_start();
 ?>
 
+<?php include "include/header.php"; ?>
 <!doctype html>
 <html lang="hu">
 <head>
@@ -14,9 +15,25 @@
 </head>
 <body>
 <div class="wrapper-div">
-    <?php include "include/header.php"; ?>
-    <div class="container" style="margin-top: 100px">
-        <h3>Hello world</h3>
+    <h2 class="text-center text-primary h2-top-center-margin">Hello!</h2>
+    <div class="container floating-div col-md-10 col-lg-6">
+        <h4 class="text-secondary">Tudnivalók az oldal használatáról</h4>
+        <ul>
+            <li>Az aloldalak eléréséhez bejelentkezés szükséges, bejelentkezni Google-fiókkal lehet</li>
+            <li>Az oldal a Googletől csak a profil alapadatait veszi át (pl. név, e-mail)</li>
+            <li>Mivel az oldalra mindenki, aki rendelkezik Google-fiókkal be tud jelentkezni, ezért biztonsági okokból
+                egyes aloldalak további jogosultságokat követelhetnek meg
+                <ul>
+                    <li>Ilyenek például a szavazós oldalak</li>
+                    <li>Az ezek eléréséhez szükséges jogosultságokat mindenki egyénileg kaphatja meg rövid időn belül</li>
+                    <li>A jogosultságaidat a <a href="profile.php">profilodon</a> ellenőrizheted</li>
+                </ul>
+            </li>
+            <li>Bizonyos oldalak csak az időpont véglegesítését követően válnak elérhetővé (pl. Résztvevők)</li>
+            <li class="mt-4">Az oldal megfelelő működését többször is teszteltük, de egy ilyen nagyméretű projektben
+                még így is előfordulhatnak hibák. Amennyiben ilyet tapasztalsz, a
+                <a href="issue_report.php">Hibabejelentés</a> oldalon jelezheted</li>
+        </ul>
     </div>
     <?php include "include/footer.php"; ?>
 </div>
